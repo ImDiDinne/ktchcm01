cb84bc8e574852f653dfba949a1fa86c86abdf8b
ImDiDinne <hnhatduy95@gmail.com>
2026-05-29 04:34:46 +0700

    Admin update zoneCfg from Web UI

--- zoneCfg.js
+++ zoneCfg.js
@@ -1,1 +1,41 @@
-var GLOBAL_ZONE_CFG = {};
+var GLOBAL_ZONE_CFG = {
+  "gate": {
+    "minPerTruck": 30
+  },
+  "cbs": {
+    "orderPerHour": 24000
+  },
+  "ck1": {
+    "pal": 100,
+    "ns": 50
+  },
+  "ck2": {
+    "pal": 100,
+    "ns": 50
+  },
+  "nv": {
+    "pal": 60,
+    "ns": 20
+  },
+  "hcm": {
+    "pal": 60,
+    "ns": 20
+  },
+  "kienPerPallet": 30,
+  "routeCap": {
+    "Kho Chuyển Tiếp": 380,
+    "Kho Giao Hàng Nặng": 270,
+    "Kho KHL": 300,
+    "Kho Trung Chuyển": 1010,
+    "Nội Thành": 1000,
+    "Nội vùng": 1400,
+    "Phú Quốc": 70
+  },
+  "childCap": {},
+  "routeTypeCap": {
+    "heavy": 400,
+    "inter": 8000,
+    "regional": 2000,
+    "city": 250
+  }
+};
